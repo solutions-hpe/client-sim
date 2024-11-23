dde39e9f308a3f935b9aeb04d7f5aaab797b6f8a
Lance Bockenstedt <lrb@hpe.com>
2024-11-23 13:33:03 -0600

    startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,8 @@ echo Waiting for startup | tee /usr/local/scripts/sim.log
 sleep 30 | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
 echo Updating Scripts | tee -a /usr/local/scripts/sim.log
-smbclient '//100.127.1.254/Public' -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
+#smbclient '//100.127.1.254/Public' -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
+sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
 #------------------------------------------------------------
 echo Setting Script Permissions | tee -a /usr/local/scripts/sim.log
 cd /usr/local/scripts/ && chmod +x *.sh
