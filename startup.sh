4ebeb96282c5ecfd3533980fc46890473b6764a3
Lance Bockenstedt <lrb@hpe.com>
2024-11-23 19:31:09 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,6 +11,7 @@ sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simula
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
+sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
 #------------------------------------------------------------
 echo Setting Script Permissions | tee -a /usr/local/scripts/sim.log
 cd /usr/local/scripts/ && sudo chmod +x *.sh
