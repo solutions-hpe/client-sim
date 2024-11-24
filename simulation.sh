1f6fd802f0a4b12901c29e40aa83a480490a2423
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 09:36:41 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -62,11 +62,12 @@ if [ $sim_phy == "ethernet" ]; then sudo ifconfig wlan0 down
 if [ $sim_phy == "wireless" ]; then sudo ifconfig eth0 down
 #------------------------------------------------------------
 #Checking to see if there is a cache device to connect to
+echo VH Server is $vh_server | tee -a /usr/local/scripts/sim.log
 if [ $vh_server == "on" ]; then
 	if [[ -e "/usr/local/scripts/vhcached.txt" ]]; then
 		#Setting Virtual Here to run as a Daemon
  		sudo /usr/local/virtualhere/vhuit64 -n
-	 	echo Waiting for VH startup | tee /usr/local/scripts/sim.log
+	 	echo Waiting for VH startup | tee -a /usr/local/scripts/sim.log
 		sleep 30
  		#Setting value to cached adapter
 		#This way the client is always using the same adapter
@@ -76,23 +77,24 @@ if [ $vh_server == "on" ]; then
 	else
 		#Counting & searching records in /tmp/vhactive.txt
 		vhactive=$(cat /tmp/vhactive.txt | grep -e -- | grep -v In-use | awk -F'[()]' '{print $2}')
-			for r in $vhactive; do
-				r_count=$((r_count+1))
-			done
-			echo VH Record Count $r_count | tee -a /usr/local/scripts/sim.log
-			#Generating random number to connect to a random adapter
-			rn_vhactive=$((1 + RANDOM % $r_count))
-			#Resetting record counter for next loop
-			r_count=0
-			#Looping through records to find an available adapter
-			for r in $vhactive; do
-				r_count=$((r_count+1))
-				if [[ $r_count == $rn_vhactive ]]; then
-					vhserver_device=$r
-					echo New VH $vhserver_device | tee -a /usr/local/scripts/sim.log
-					echo $vhserver_device | tee /usr/local/scripts/vhcached.txt
-				fi
-			done
+		for r in $vhactive; do
+			r_count=$((r_count+1))
+		done
+		echo VH Record Count $r_count | tee -a /usr/local/scripts/sim.log
+		#Generating random number to connect to a random adapter
+		rn_vhactive=$((1 + RANDOM % $r_count))
+		#Resetting record counter for next loop
+		r_count=0
+		#Looping through records to find an available adapter
+		for r in $vhactive; do
+			r_count=$((r_count+1))
+			if [[ $r_count == $rn_vhactive ]]; then
+				vhserver_device=$r
+				echo New VH $vhserver_device | tee -a /usr/local/scripts/sim.log
+				echo $vhserver_device | tee /usr/local/scripts/vhcached.txt
+			fi
+		done
+   	fi
 fi
 #------------------------------------------------------------
 
