3be9a236d39972dc3ce1efb51bd9e17764f8af8e
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 16:27:37 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,7 +17,7 @@ vh_server=$(get_value 'simulation' 'vh_server')
 smb_address=$(get_value 'address' 'smb_address')
 #------------------------------------------------------------
 #Making sure eth0 and wlan0 are online
-echo Bringing all interfaces online | tee -a /usr/local/scripts/sim.log
+echo Bringing up all interfaces online | tee -a /usr/local/scripts/sim.log
 sudo ifconfig eth0 up
 sudo ifconfig wlan0 up
 #Sleeping for 30 seconds to bring up network interaces
