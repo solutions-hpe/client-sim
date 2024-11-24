b9f49077cd68a3e2b4b639814f6aca167374ff49
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 16:57:36 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.14
+version=.15
 echo $(date) | tee -a /usr/local/scripts/sim.log
 echo --------------------------| tee -a /usr/local/scripts/sim.log
 echo Simulation Script Version $version | tee -a /usr/local/scripts/sim.log
@@ -238,9 +238,10 @@ if [ $kill_switch == "off" ]; then
 		#Running download simulation
 		if [ $download == "on" ]; then
 				echo Running download simulation | tee -a /usr/local/scripts/sim.log
-				curl -o /tmp/Ubuntu.gz http://archive.ubuntu.com/ubuntu/dists/bionic/Contents-i386.gz
-				curl -o /tmp/PDF10MB.pdf https://link.testfile.org/PDF10MB
-				curl -o /tmp/Test.mp3 https://files.testfile.org/anime.mp3	
+				curl -o /tmp/Ubuntu.gz http://archive.ubuntu.com/ubuntu/dists/bionic/Contents-i386.gz | tee -a /usr/local/scripts/sim.log 
+				curl -o /tmp/main.cvd https://packages.microsoft.com/clamav/main.cvd | tee -a /usr/local/scripts/sim.log
+				curl -o /tmp/manifest https://android.googlesource.com/platform/manifest | tee -a /usr/local/scripts/sim.log
+    				curl -0 /tmp/bootcamp5.1.5769.zip https://download.info.apple.com/Mac_OS_X/031-30890-20150812-ea191174-4130-11e5-a125-930911ba098f/bootcamp5.1.5769.zip| tee -a /usr/local/scripts/sim.log
 		fi
 		#Running apt update & apt upgrade
 		echo Running Updates | tee -a /usr/local/scripts/sim.log
