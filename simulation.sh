5bc2fe7fe3d3bef649217309348ee3889fa8f873
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 15:20:10 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -77,7 +77,7 @@ vh_server_address=$(get_value 'address' 'vh_server_addr')
 rn=$((1 + RANDOM % 60))
 #------------------------------------------------------------
 echo Disabling unused interface | tee -a /usr/local/scripts/sim.log
-if [ $sim_phy == "ethernet" ]; then echo ethernet
+#if [ $sim_phy == "ethernet" ]; then echo ethernet
 if [ $sim_phy == "wireless" ]; then echo wireless
 #------------------------------------------------------------
 #Checking to see if there is a cache device to connect to
