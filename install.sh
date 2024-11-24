e6489b49a2c88763bdf55ba292e7b9fc3bf88220
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 08:02:53 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startu
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
+sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.conf -O /usr/local/scripts/simulation.conf
 #------------------------------------------------------------
