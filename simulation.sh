80c501314d3a030b0f64f299e5091951f2218760
Lance Bockenstedt <lrb@hpe.com>
2024-11-23 19:31:42 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -159,7 +159,8 @@ if [ $kill_switch == "off" ]; then
 	echo Updating Scripts | tee -a /usr/local/scripts/sim.log
 	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
 	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
-	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
+	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
+ 	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
 	sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
 	#------------------------------------------------------------
  	#smbclient $smb_location -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
