a313a1674eb54073b56a0a0c7dfc9c668228ced5
Lance Bockenstedt <lrb@hpe.com>
2024-11-24 09:28:11 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -108,98 +108,15 @@ sleep 30 | tee -a /usr/local/scripts/sim.log
 #Checking for kill switch to stop simulation
 if [ $kill_switch == "off" ]; then
 	for z in {1..100}; do
-	#------------------------------------------------------------
-	#Setting up Client Simulations
-	if [[ $HOSTNAME == "SIM-LNX-200"* ]]; then wsite=MIA && sim=DNS; fi
- 	if [[ $HOSTNAME == "SIM-RPI-"* ]]; then wsite=MIA && sim=DNS; fi
-	#End Setting up Client Simulations
-	#------------------------------------------------------------
-
-	#------------------------------------------------------------ 
-	#Logging Simulation
-	echo --------------------------| tee -a /usr/local/scripts/sim.log
-	echo Simulation Details: | tee -a /usr/local/scripts/sim.log
-	echo Hostname: $HOSTNAME | tee -a /usr/local/scripts/sim.log
-	echo Site: $wsite | tee -a /usr/local/scripts/sim.log
-	echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
-	echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
-	echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
-	echo DHCP Fail: $dhcp_fail | tee -a /usr/local/scripts/sim.log
-	echo DNS Fail: $dns_fail | tee -a /usr/local/scripts/sim.log
-	echo WWW Traffic: $www_traffic | tee -a /usr/local/scripts/sim.log
-	echo Port Flap: $port_flap | tee -a /usr/local/scripts/sim.log
-	echo --------------------------| tee -a /usr/local/scripts/sim.log
-	#------------------------------------------------------------ 
-
-	#------------------------------------------------------------
-	#Connecting to Network
-	nmcli dev wifi connect $wsite-PSK password $ssidpw | tee -a /usr/local/scripts/sim.log
-	echo Waiting for Network | tee -a /usr/local/scripts/sim.log
-	sleep 30 | tee -a /usr/local/scripts/sim.log
-	#End Connecting to Network
-	#------------------------------------------------------------
-
-	#------------------------------------------------------------
-	#Running WWW Traffic Simulation
-	if [ $www_traffic == "on" ]; then
-		r_count=0
-		echo Running WWW Traffic simulation
-		wwwfile=$(cat /usr/local/scripts/websites.txt)
-			for r in $wwwfile; do
-				r_count=$((r_count+1))
-			done
-			rn_www=$((1 + RANDOM % $r_count))
-			r_count=0
-			for r in $wwwfile; do
-				r_count=$((r_count+1))
-				if [[ $r_count == $rn_www ]]; then
-					echo Closing Firefox | tee -a /usr/local/scripts/sim.log
-					pkill -f firefox
-					sleep 1
-					echo --------------------------| tee -a /usr/local/scripts/sim.log
-					echo --------------------------| tee -a /usr/local/scripts/sim.log
-					echo Simulation Details: | tee -a /usr/local/scripts/sim.log
-					echo Hostname: $HOSTNAME | tee -a /usr/local/scripts/sim.log
-					echo Site: $wsite | tee -a /usr/local/scripts/sim.log
-					echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
-					echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
-					echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
-					echo WWW Sim: $www_traffic | tee -a /usr/local/scripts/sim.log
-					echo WWW Traffic Generation: | tee -a /usr/local/scripts/sim.log
-					echo Website: $r | tee -a /usr/local/scripts/sim.log
-					echo --------------------------| tee -a /usr/local/scripts/sim.log
-					echo --------------------------| tee -a /usr/local/scripts/sim.log
-					firefox --headless --newtab $r &
-					#firefox --new-tab $r &
-					www_traffic=off
-				fi
-			done
-		fi
-	#End WWW Traffic Simulation
-	#------------------------------------------------------------	
- 
-	#------------------------------------------------------------
-	#Updating Scripts
-	echo Updating Scripts | tee -a /usr/local/scripts/sim.log
- 	if [ $public_repo == "on" ]; then
-  		#Using remote GitHub repo
-		sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
-		sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
-		sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
- 		sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
-		sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
- 	else
-  		#Local repo defined in the conf file
-  		smbclient $smb_location -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
-  	fi
-	#------------------------------------------------------------
-	#End Updating Scripts
-	#------------------------------------------------------------
- 
-	#------------------------------------------------------------
-	#Running ping simulation
-	if [ $ping_test == "on" ]; then
-		echo --------------------------| tee -a /usr/local/scripts/sim.log
+		#------------------------------------------------------------
+		#Setting up Client Simulations
+		if [[ $HOSTNAME == "SIM-LNX-200"* ]]; then wsite=MIA && sim=DNS; fi
+ 		if [[ $HOSTNAME == "SIM-RPI-"* ]]; then wsite=MIA && sim=DNS; fi
+		#End Setting up Client Simulations
+		#------------------------------------------------------------	
+	
+		#------------------------------------------------------------ 
+		#Logging Simulation
 		echo --------------------------| tee -a /usr/local/scripts/sim.log
 		echo Simulation Details: | tee -a /usr/local/scripts/sim.log
 		echo Hostname: $HOSTNAME | tee -a /usr/local/scripts/sim.log
@@ -207,40 +124,38 @@ if [ $kill_switch == "off" ]; then
 		echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
 		echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
 		echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
-		echo Ping Sim: $ping_test | tee -a /usr/local/scripts/sim.log
-		echo Ping Address: $ping_address | tee -a /usr/local/scripts/sim.log
-		echo Count: $rn | tee -a /usr/local/scripts/sim.log
+		echo DHCP Fail: $dhcp_fail | tee -a /usr/local/scripts/sim.log
+		echo DNS Fail: $dns_fail | tee -a /usr/local/scripts/sim.log
+		echo WWW Traffic: $www_traffic | tee -a /usr/local/scripts/sim.log
+		echo Port Flap: $port_flap | tee -a /usr/local/scripts/sim.log
 		echo --------------------------| tee -a /usr/local/scripts/sim.log
-		echo --------------------------| tee -a /usr/local/scripts/sim.log
-		echo Running ping simulation
-		echo Pinging Default Gateway
-		ping -c $rn $ping_address
-	fi
-	#End Ping Simulation
-	#------------------------------------------------------------
+		#------------------------------------------------------------ 
 
- 	#------------------------------------------------------------
-	#Running download simulation
-	if [ $download == "on" ]; then
-			echo Running download simulation | tee -a /usr/local/scripts/sim.log
-			curl -o /tmp/Ubuntu.gz http://archive.ubuntu.com/ubuntu/dists/bionic/Contents-i386.gz
-			curl -o /tmp/PDF10MB.pdf https://link.testfile.org/PDF10MB
-			curl -o /tmp/Test.mp3 https://files.testfile.org/anime.mp3	
-	fi
-	#Running apt update & apt upgrade
-	echo Running Updates | tee -a /usr/local/scripts/sim.log
-	sudo apt update 
-	sudo apt upgrade -y 
-	#End Download Simulation
-	#------------------------------------------------------------
+		#------------------------------------------------------------
+		#Connecting to Network
+		nmcli dev wifi connect $wsite-PSK password $ssidpw | tee -a /usr/local/scripts/sim.log
+		echo Waiting for Network | tee -a /usr/local/scripts/sim.log
+		sleep 30 | tee -a /usr/local/scripts/sim.log
+		#End Connecting to Network
+		#------------------------------------------------------------
 
-	#------------------------------------------------------------
-	#Running DNS Fail simulation
-	if [ $dns_fail == "on" ]; then
-			dnsfile=$(cat /usr/local/scripts/dns_fail.txt)
-			for i in {1..100}; do
-				for r in $dnsfile
-					do
+		#------------------------------------------------------------
+		#Running WWW Traffic Simulation
+		if [ $www_traffic == "on" ]; then
+			r_count=0
+			echo Running WWW Traffic simulation
+			wwwfile=$(cat /usr/local/scripts/websites.txt)
+				for r in $wwwfile; do
+					r_count=$((r_count+1))
+				done
+				rn_www=$((1 + RANDOM % $r_count))
+				r_count=0
+				for r in $wwwfile; do
+					r_count=$((r_count+1))
+					if [[ $r_count == $rn_www ]]; then
+						echo Closing Firefox | tee -a /usr/local/scripts/sim.log
+						pkill -f firefox
+						sleep 1
 						echo --------------------------| tee -a /usr/local/scripts/sim.log
 						echo --------------------------| tee -a /usr/local/scripts/sim.log
 						echo Simulation Details: | tee -a /usr/local/scripts/sim.log
@@ -249,36 +164,119 @@ if [ $kill_switch == "off" ]; then
 						echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
 						echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
 						echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
-						echo DNS Fail: $dns_fail | tee -a /usr/local/scripts/sim.log
-						echo Running DNS Failure: | tee -a /usr/local/scripts/sim.log
-						echo Simulation Iteration: $i | tee -a /usr/local/scripts/sim.log
-						echo $r | tee -a /usr/local/scripts/sim.log
+						echo WWW Sim: $www_traffic | tee -a /usr/local/scripts/sim.log
+						echo WWW Traffic Generation: | tee -a /usr/local/scripts/sim.log
+						echo Website: $r | tee -a /usr/local/scripts/sim.log
 						echo --------------------------| tee -a /usr/local/scripts/sim.log
 						echo --------------------------| tee -a /usr/local/scripts/sim.log
-						dig @$dns_bad_record_1 $r &
-						dig @$dns_bad_record_2 $r &
-						dig @$dns_bad_record_3 $r &
-						dig @$dns_bad_ip_1 $r &
-						dig @$dns_bad_ip_2 $r &
-						dig @$dns_bad_ip_3 $r &
-						dig @$dns_latency_1 $r &
-						dig @$dns_latency_2 $r &
-						dig @$dns_latency_3 $r &
-						wait
-					done
-			done
+						firefox --headless --newtab $r &
+						#firefox --new-tab $r &
+						www_traffic=off
+					fi
+				done
+			fi
+		#End WWW Traffic Simulation
+		#------------------------------------------------------------	
+	 
+		#------------------------------------------------------------
+		#Updating Scripts
+		echo Updating Scripts | tee -a /usr/local/scripts/sim.log
+ 		if [ $public_repo == "on" ]; then
+  			#Using remote GitHub repo
+			sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
+			sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
+			sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
+ 			sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websits.txt
+			sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
+ 		else
+  			#Local repo defined in the conf file
+  			smbclient $smb_location -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
+  		fi
+		#------------------------------------------------------------
+		#End Updating Scripts
+		#------------------------------------------------------------
+	 
+		#------------------------------------------------------------
+		#Running ping simulation
+		if [ $ping_test == "on" ]; then
+			echo --------------------------| tee -a /usr/local/scripts/sim.log
+			echo --------------------------| tee -a /usr/local/scripts/sim.log
+			echo Simulation Details: | tee -a /usr/local/scripts/sim.log
+			echo Hostname: $HOSTNAME | tee -a /usr/local/scripts/sim.log
+			echo Site: $wsite | tee -a /usr/local/scripts/sim.log
+			echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
+			echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
+			echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
+			echo Ping Sim: $ping_test | tee -a /usr/local/scripts/sim.log
+			echo Ping Address: $ping_address | tee -a /usr/local/scripts/sim.log
+			echo Count: $rn | tee -a /usr/local/scripts/sim.log
+			echo --------------------------| tee -a /usr/local/scripts/sim.log
+			echo --------------------------| tee -a /usr/local/scripts/sim.log
+			echo Running ping simulation
+			echo Pinging Default Gateway
+			ping -c $rn $ping_address
 		fi
-	#End DNS Fail Simulation
+		#End Ping Simulation
+		#------------------------------------------------------------
+ 		#------------------------------------------------------------
+		#Running download simulation
+		if [ $download == "on" ]; then
+				echo Running download simulation | tee -a /usr/local/scripts/sim.log
+				curl -o /tmp/Ubuntu.gz http://archive.ubuntu.com/ubuntu/dists/bionic/Contents-i386.gz
+				curl -o /tmp/PDF10MB.pdf https://link.testfile.org/PDF10MB
+				curl -o /tmp/Test.mp3 https://files.testfile.org/anime.mp3	
+		fi
+		#Running apt update & apt upgrade
+		echo Running Updates | tee -a /usr/local/scripts/sim.log
+		sudo apt update 
+		sudo apt upgrade -y 
+		#End Download Simulation
+		#------------------------------------------------------------
+		#------------------------------------------------------------
+		#Running DNS Fail simulation
+		if [ $dns_fail == "on" ]; then
+				dnsfile=$(cat /usr/local/scripts/dns_fail.txt)
+				for i in {1..100}; do
+					for r in $dnsfile
+						do
+							echo --------------------------| tee -a /usr/local/scripts/sim.log
+							echo --------------------------| tee -a /usr/local/scripts/sim.log
+							echo Simulation Details: | tee -a /usr/local/scripts/sim.log
+							echo Hostname: $HOSTNAME | tee -a /usr/local/scripts/sim.log
+							echo Site: $wsite | tee -a /usr/local/scripts/sim.log
+							echo Simulation: $sim | tee -a /usr/local/scripts/sim.log
+							echo Simulation Load: $sim_load | tee -a /usr/local/scripts/sim.log
+							echo Kill Switch: $kill_switch | tee -a /usr/local/scripts/sim.log
+							echo DNS Fail: $dns_fail | tee -a /usr/local/scripts/sim.log
+							echo Running DNS Failure: | tee -a /usr/local/scripts/sim.log
+							echo Simulation Iteration: $i | tee -a /usr/local/scripts/sim.log
+							echo $r | tee -a /usr/local/scripts/sim.log
+							echo --------------------------| tee -a /usr/local/scripts/sim.log
+							echo --------------------------| tee -a /usr/local/scripts/sim.log
+							dig @$dns_bad_record_1 $r &
+							dig @$dns_bad_record_2 $r &
+							dig @$dns_bad_record_3 $r &
+							dig @$dns_bad_ip_1 $r &
+							dig @$dns_bad_ip_2 $r &
+							dig @$dns_bad_ip_3 $r &
+							dig @$dns_latency_1 $r &
+							dig @$dns_latency_2 $r &
+							dig @$dns_latency_3 $r &
+							wait
+						done
+				done
+			fi
+		#End DNS Fail Simulation
+		#------------------------------------------------------------
+		echo End of simulation sleeping for 5 seconds
+		sleep 5
+	done
+	else
+		#------------------------------------------------------------
+		#If kill switch is enabled - sleeping for 5 minutes then restarting the loop
+		echo Kill switch enabled - sleeping for 5 minutes
+		sleep 300
+	fi
 	#------------------------------------------------------------
-	echo End of simulation sleeping for 5 seconds
-	sleep 5
-done
-else
-	#------------------------------------------------------------
-	#If kill switch is enabled - sleeping for 5 minutes then restarting the loop
-	echo Kill switch enabled - sleeping for 5 minutes
-	sleep 300
-fi
-#------------------------------------------------------------
-#Looping Script
-source /usr/local/scripts/simulation.sh
+	#Looping Script
+	source /usr/local/scripts/simulation.sh
