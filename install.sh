ae2a2e1a38dab120abb2b10d6da02c2a19444b9c
Lance Bockenstedt <lrb@hpe.com>
2024-11-25 20:28:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo enabling no password for sudo for current user
 echo "$USER   ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo making scripts directory
 sudo mkdir /usr/local/scripts
-sudo chmod -R 666 /usr/local/scripts
+sudo chmod -R 777 /usr/local/scripts
 #------------------------------------------------------------
 echo Downloading scripts from source on GitHub
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
