bcc404d5ce1bac9f56e3aeed154475bc81e4c9c8
Lance Bockenstedt <lrb@hpe.com>
2024-12-02 15:12:12 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,19 +26,19 @@ git clone https://github.com/morrownr/88x2bu-20210702.git
 #------------------------------------------------------------
 echo Installing Network Adapter Drivers
 cd 8821au-20210708
-sudo ./install.sh
+sudo ./install.sh NoPrompt
 cd ..
 cd 8821cu-20210916
-sudo ./install.sh
+sudo ./install.sh NoPrompt
 cd ..
 cd 8814
-sudo ./install.sh
+sudo ./install.sh NoPrompt
 cd ..
 cd 8812au-20210820
-sudo ./install.sh
+sudo ./install.sh NoPrompt
 cd ..
 cd 88x2bu-20210702
-sudo ./install.sh
+sudo ./install.sh NoPrompt
 #------------------------------------------------------------
 #Installing SMBClient to sync with local CIFS repo
 sudo apt update
