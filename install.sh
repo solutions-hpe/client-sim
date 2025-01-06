0be0823ef23af54d791b4eb09fe544c4404093c6
Lance Bockenstedt <lrb@hpe.com>
2025-01-06 09:16:44 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,6 +78,6 @@ echo Comment=Simulation Script Startup | sudo tee -a /etc/xdg/autostart/logview.
 echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
 #Ubuntu/Debian with gnome
 echo Exec=gnome-terminal --geometry=15x15+0+477 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
-
+echo Exec=gnome-terminal --geometry=95x15+1400+477 -- journalctrl -f | sudo tee -a /etc/xdg/autostart/logview.desktop
 #End Log Viewer
 #------------------------------------------------------------
