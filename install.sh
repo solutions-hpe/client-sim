5cb2d3238d9d9a30746083dbc1c198ce64d90f59
Lance Bockenstedt <lrb@hpe.com>
2025-01-09 11:08:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,11 +65,12 @@ echo Downloading scripts from source on GitHub | tee -a /tmp/client-sim.log
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
-sudo chmod -R 777 /usr/local/scripts
+sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/sim-update.sh -O /usr/local/scripts/sim-update.sh
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websites.txt
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.conf -O /usr/local/scripts/simulation.conf
 touch /usr/local/scripts/sim.log
+sudo chmod -R 777 /usr/local/scripts
 #------------------------------------------------------------
 echo Getting Network Adapter Drivers from GitHub | tee -a /tmp/client-sim.log
 git clone https://github.com/morrownr/8821au-20210708.git
