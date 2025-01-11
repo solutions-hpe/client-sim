b486460e7c378203a952ff15041665307ef0834b
Lance Bockenstedt <lrb@hpe.com>
2025-01-11 11:27:46 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.13
+version=.14
 echo --------------------------| tee /usr/local/scripts/sim.log
 echo Startup Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
