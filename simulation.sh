41267e26ca8588c05d7a6128b02b021e28369449
Lance Bockenstedt <lrb@hpe.com>
2025-01-11 06:55:28 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -72,51 +72,7 @@ echo Disabling unused interface | tee -a /usr/local/scripts/sim.log
 if [ $sim_phy == "ethernet" ]; then sudo ifconfig $wladapter down; fi
 if [ $sim_phy == "wireless" ] && [ $vh_server == "off" ]; then sudo ifconfig $eadapter down; fi
 #------------------------------------------------------------
-#Checking to see if there is a cache device to connect to
-echo VH Server is $vh_server | tee -a /usr/local/scripts/sim.log
-if [ $vh_server == "on" ]; then
-	if [[ -e "/usr/local/scripts/vhcached.txt" ]]; then
- 		#Setting value to cached adapter
-		#This way the client is always using the same adapter
-		#Otherwise connectivity for clients will have gaps when the adapter changes in Central
-		vhserver_device=$(cat /usr/local/scripts/vhcached.txt)
-		echo Cached $vhserver_device | tee -a /usr/local/scripts/sim.log
-  	#If VirtualHere cached value does not exist
-   	else
-		#Counting & searching records in /tmp/vhactive.txt
-		vhactive=$(cat /tmp/vhactive.txt | grep -e -- | grep -v In-use | awk -F'[()]' '{print $2}')
-		for r in $vhactive; do
-			r_count=$((r_count+1))
-		done
-		echo VH Record Count $r_count | tee -a /usr/local/scripts/sim.log
-		#Generating random number to connect to a random adapter
-		rn_vhactive=$((1 + RANDOM % $r_count))
-		#Resetting record counter for next loop
-		r_count=0
-		#Looping through records to find an available adapter
-		for r in $vhactive; do
-			r_count=$((r_count+1))
-			if [[ $r_count == $rn_vhactive ]]; then
-				vhserver_device=$r
-				echo New VH $vhserver_device | tee -a /usr/local/scripts/sim.log
-				echo $vhserver_device | tee /usr/local/scripts/vhcached.txt
-			fi
-		done
-   	#End if VirtualHere cached value check
-    	fi
-	#End Checking to see if there is a cache device to connect to
-	#------------------------------------------------------------
-	#------------------------------------------------------------
-	#Connecting to VirtualHere Server
-	echo Connecting to USB Adapter | tee -a /usr/local/scripts/sim.log
-	#Connecting to Adapter
-	/usr/sbin/vhclientx86_64 -t AUTO USE,$vhserver_device | tee -a /usr/local/scripts/sim.log
-	#End Connecting to VirtualHere Server
-	#------------------------------------------------------------
-	echo Waiting for Adapter | tee -a /usr/local/scripts/sim.log
-	sleep 30
-#End if VirtualHere Server is enabled
-fi
+
 #------------------------------------------------------------
 #Checking for kill switch to stop simulation
 if [ $kill_switch == "off" ]; then
