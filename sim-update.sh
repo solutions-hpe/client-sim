cb564136428fc2ae8f3992359739f330e9ccd828
Lance Bockenstedt <lrb@hpe.com>
2025-01-11 07:27:34 -0600

    Update sim-update.sh

--- sim-update.sh
+++ sim-update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 version=.03
 github=raw.githubusercontent.com
-if [ ping -c1 $github &>/dev/null ]; then
-  sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
-  sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
-  sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
-  sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websites.txt
-  sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
-  sudo chmod -R 777 /usr/local/scripts
-else
-  echo Network connection failed to GitHub - skipping script updates
-fi
+while true;
+do 
+  ping -c1 $github
+  if [ $? -eq 0 ]; then
+    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
+    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
+    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
+    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websites.txt
+    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
+    sudo chmod -R 777 /usr/local/scripts
+    exit 0
+  else
+    echo Network connection failed to GitHub - skipping script updates
+    exit 0
+  fi
+done
