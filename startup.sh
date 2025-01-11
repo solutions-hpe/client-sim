e12cec842b8e45891c06a6d70524eef2df73a8a9
Lance Bockenstedt <lrb@hpe.com>
2025-01-11 16:53:57 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -60,52 +60,6 @@ if [ $vh_server == "on" ]; then
   sudo /usr/sbin/vhclientx86_64 -t LIST -r /tmp/vhactive.txt
 fi
 #------------------------------------------------------------
-#Checking to see if there is a cache device to connect to
-echo VH Server is $vh_server | tee -a /usr/local/scripts/sim.log
-if [ $vh_server == "on" ]; then
-	if [ -e "/usr/local/scripts/vhcached.txt" ]; then
- 		#Setting value to cached adapter
-		#This way the client is always using the same adapter
-		#Otherwise connectivity for clients will have gaps when the adapter changes in Central
-		vhserver_device=$(cat /usr/local/scripts/vhcached.txt)
-		echo Cached $vhserver_device | tee -a /usr/local/scripts/sim.log
-  	#If VirtualHere cached value does not exist
-   	else
-		#Counting & searching records in /tmp/vhactive.txt
-		vhactive=$(cat /tmp/vhactive.txt | grep -e -- | grep -v In-use | awk -F'[()]' '{print $2}')
-		for r in $vhactive; do
-			r_count=$((r_count+1))
-		done
-		echo VH Record Count $r_count | tee -a /usr/local/scripts/sim.log
-		#Generating random number to connect to a random adapter
-		rn_vhactive=$((1 + RANDOM % $r_count))
-		#Resetting record counter for next loop
-		r_count=0
-		#Looping through records to find an available adapter
-		for r in $vhactive; do
-			r_count=$((r_count+1))
-			if [[ $r_count == $rn_vhactive ]]; then
-				vhserver_device=$r
-				echo New VH $vhserver_device | tee -a /usr/local/scripts/sim.log
-				echo $vhserver_device | tee /usr/local/scripts/vhcached.txt
-			fi
-		done
-   	#End if VirtualHere cached value check
-    	fi
-	#End Checking to see if there is a cache device to connect to
-	#------------------------------------------------------------
-	#------------------------------------------------------------
-	#Connecting to VirtualHere Server
-	echo Connecting to USB Adapter | tee -a /usr/local/scripts/sim.log
-	#Connecting to Adapter
-	/usr/sbin/vhclientx86_64 -t "AUTO USE DEVICE,$vhserver_device" | tee -a /usr/local/scripts/sim.log
-	#End Connecting to VirtualHere Server
-	#------------------------------------------------------------
-	echo Waiting for Adapter | tee -a /usr/local/scripts/sim.log
-	sleep 30
-#End if VirtualHere Server is enabled
-fi
-#------------------------------------------------------------
 #Updating Scripts
 echo Updating Scripts | tee -a /usr/local/scripts/sim.log
 echo Checking connectivity to GitHub | tee -a /usr/local/scripts/sim.log
