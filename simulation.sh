73f4ceaed2743d729224c02ea0dfe42a9adf07ac
Lance Bockenstedt <lrb@hpe.com>
2025-01-11 07:35:44 -0600

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -155,16 +155,25 @@ if [ $kill_switch == "off" ]; then
 		echo Updating Scripts | tee -a /usr/local/scripts/sim.log
   		echo Checking connectivity to GitHub | tee -a /usr/local/scripts/sim.log
     		github=raw.githubusercontent.com
- 		if [ $public_repo == "on" ] && [ ping -c1 $github 1>/dev/null 2>/dev/null ]; then
-  			#Using remote GitHub repo
-			sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
-			sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
-			sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
- 			sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websites.txt
-			sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
- 		else
-  			#Local repo defined in the conf file
-  			smbclient $smb_location -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
+ 		if [ $public_repo == "on" ]; then
+  		#Using remote GitHub repo
+     		 while true;
+		 do 
+  		  ping -c1 $github
+  		   if [ $? -eq 0 ]; then
+    		    echo Successful network connection to Github - updating scripts
+		    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/simulation.sh -O /usr/local/scripts/simulation.sh
+		    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/startup.sh -O /usr/local/scripts/startup.sh
+		    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/ini-parser.sh -O /usr/local/scripts/ini-parser.sh
+ 		    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/websites.txt -O /usr/local/scripts/websites.txt
+		    sudo wget -4 https://raw.githubusercontent.com/solutions-hpe/client-sim/main/dns_fail.txt -O /usr/local/scripts/dns_fail.txt
+    		   else
+    		    echo Network connection failed to GitHub - skipping script updates
+		  fi
+   		 done
+ 		  else
+  		  #Local repo defined in the conf file
+  		  smbclient $smb_location -c 'lcd /usr/local/scripts/; cd Scripts; prompt; mget *' -N
   		fi
 		#------------------------------------------------------------
 		#End Updating Scripts
