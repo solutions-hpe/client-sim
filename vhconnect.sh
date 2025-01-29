6b76a9a27171f5f3b035ade478cb15399802a17b
Lance Bockenstedt <lrb@hpe.com>
2025-01-29 13:08:54 -0600

    update

--- vhconnect.sh
+++ vhconnect.sh
@@ -58,7 +58,7 @@ if [ $vh_server == "on" ]; then
 	#Connecting to VirtualHere Server
 	echo Connecting to USB Adapter | tee -a /usr/local/scripts/sim.log
 	#Connecting to Adapter
-	/usr/sbin/vhclientx86_64 -t "AUTO USE DEVICE,$vhserver_device" | tee -a /usr/local/scripts/sim.log
+	/usr/sbin/vhclientx86_64 -t "AUTO USE DEVICE,$vhserver_device"
 	#End Connecting to VirtualHere Server
 	#------------------------------------------------------------
 	echo Waiting for Adapter | tee -a /usr/local/scripts/sim.log
