43137a1cb91cabfc9e76ec6fdc14c073793e6f15
Lance Bockenstedt <lrb@hpe.com>
2025-03-21 14:20:37 -0500

    Update wireless.sh

--- wireless.sh
+++ wireless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Script Version .03" | tee /usr/scripts/wireless.log
+echo "Script Version .04" | tee /usr/scripts/wireless.log
 echo "Starting Wireless Simulations" | tee -a /usr/scripts/wireless.log
 #Scheduled Reboot
 sudo shutdown -r +45000
@@ -77,24 +77,24 @@ sudo ifconfig wlp6s16 down
     done
 #--------------------------------------------------------------------------------------------------------   
    echo "Running DHCP Simulation" | tee -a /usr/scripts/wireless.log
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan1 -c 60,str,"MercurySD" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan2 -c 60,str,"LiftMaster" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan3 -c 60,str,"RingDevice" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan4 -c 60,str,"AppleIPad" -l 060301790f6c7277fc
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan5 -c 60,str,"SamsungTV" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan6 -c 60,str,"SONOS" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan7 -c 60,str,"Hewlett-Packard JetDirect" -l 0603010f42430d2c770c51fc
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan8 -c 60,str,"PolycomIPPhone" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan9 -c 60,str,"AXIS,NetworkCamera,P3375-V,7.25.1.1" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan11 -c 60,str,"AppleIPhone" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan12 -c 60,str,"RingDevice" -l 0103061c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan13 -c 60,str,"Resideo" -l 010306
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan14 -c 60,str,"BarcoShare" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan15 -c 60,str,"WePresentGW" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan16 -c 60,str,"DensitySensor" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan17 -c 60,str,"Meta OculusVR" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan18 -c 60,str,"Tesla,Inc" -l 0103061afc2a0f0c
-   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan19 -c 60,str,"udhcp 1.4.2" -l 0103060c0f1c28292a7d
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan1 -c 60,str,"MercurySD" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan2 -c 60,str,"LiftMaster" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan3 -c 60,str,"RingDevice" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan4 -c 60,str,"AppleIPad" -l 0601790f6c7277fc
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan5 -c 60,str,"SamsungTV" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan6 -c 60,str,"SONOS" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan7 -c 60,str,"Hewlett-Packard JetDirect" -l 06010f42430d2c770c51fc
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan8 -c 60,str,"PolycomIPPhone" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan9 -c 60,str,"AXIS,NetworkCamera,P3375-V,7.25.1.1" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan11 -c 60,str,"AppleIPhone" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan12 -c 60,str,"RingDevice" -l 01061c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan13 -c 60,str,"Resideo" -l 0106
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan14 -c 60,str,"BarcoShare" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan15 -c 60,str,"WePresentGW" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan16 -c 60,str,"DensitySensor" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan17 -c 60,str,"Meta OculusVR" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan18 -c 60,str,"Tesla,Inc" -l 01061afc2a0f0c
+   sudo /usr/scripts/dhtest/dhtest -V -f -i vwlan19 -c 60,str,"udhcp 1.4.2" -l 01060c0f1c28292a7d
 #--------------------------------------------------------------------------------------------------------
   sudo ifconfig enp6s18 down 
   echo "Step 3 - Running Simulations" | tee -a /usr/scripts/wireless.log
