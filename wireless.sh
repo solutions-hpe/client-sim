4c42d3094ccc3b4132a2c893be2deb2cc1a84513
Lance Bockenstedt <lrb@hpe.com>
2025-03-23 19:51:56 -0500

    Update wireless.sh

--- wireless.sh
+++ wireless.sh
@@ -2,7 +2,7 @@
 echo "Script Version .27" | tee /usr/scripts/wireless.log
 echo "Starting DHCPCD Daemon" | tee -a /usr/scripts/wireless.log
 sudo dhcpcd
-sleep 30
+sleep 5
 sudo ifmetric enp6s18 10
 echo "Updating Simulation Script" | tee -a /usr/scripts/wireless.log 
 sudo wget https://raw.githubusercontent.com/solutions-hpe/client-sim/main/wireless.sh -O /tmp/wireless.sh
