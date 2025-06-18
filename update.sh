f6d27da7c0f027ac1c7be886666c1f26238cf824
Lance Bockenstedt <lrb@hpe.com>
2025-06-18 08:16:26 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uversion=.04
+uversion=.05
 echo --------------------------| tee /usr/local/scripts/sim.log
 echo VHConnect Script Version $uversion | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
