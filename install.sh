76cb72c5c3ea9cad78aa32d82c4b873f80d25e30
Lance Bockenstedt <lrb@hpe.com>
2025-06-18 12:36:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.31
+version=.32
 touch /tmp/client-sim.log
 echo Installer Version $version | tee /tmp/client-sim.log
 gnome-terminal --geometry=80x15+0+477 -- tail -f /tmp/client-sim.log
