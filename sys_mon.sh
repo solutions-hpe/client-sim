5ca8d20ac1dc44a1f4755cb0e1e5f30769df0073
Lance Bockenstedt <lrb@hpe.com>
2025-07-08 15:19:29 -0500

    Update sys_mon.sh

--- sys_mon.sh
+++ sys_mon.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-version=.04
-echo --------------------------| tee /usr/local/scripts/sim_reboot.log
+version=.05
+echo --------------------------| tee -a /usr/local/scripts/sim_reboot.log
 echo System Monitor Script Version $version | tee -a /usr/local/scripts/sim_reboot.log
-echo $(date) | tee -a /usr/local/scripts/sim_reboot.log
 echo --------------------------| tee -a /usr/local/scripts/sim_reboot.log
 LOG_FILE="/var/log/messages"  # Replace with the actual log file path
 KEYWORD="Call Trace:"  # Replace with the message to trigger the reboot
@@ -11,6 +10,8 @@ tail -f $LOG_FILE | while read LOGLINE; do
     if [[ "$LOGLINE" == *"$KEYWORD"* ]]; then
         echo "Failure message Found" | tee -a /usr/local/scripts/sim_reboot.log
         echo "Rebooting system" | tee -a /usr/local/scripts/sim_reboot.log
+        echo $(date) | tee -a /usr/local/scripts/sim_reboot.log
+        echo --------------------------| tee -a /usr/local/scripts/sim_reboot.log
         reboot
     fi
 done
