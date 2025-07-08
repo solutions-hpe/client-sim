d7f6d68159f96c62efec98222b8113d5542aea57
Lance Bockenstedt <lrb@hpe.com>
2025-07-08 09:55:17 -0500

    Update sys_mon.sh

--- sys_mon.sh
+++ sys_mon.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+version=.01
+echo --------------------------| tee /usr/local/scripts/sim.log
+echo System Monitor Script Version $version | tee -a /usr/local/scripts/sim.log
+echo $(date) | tee -a /usr/local/scripts/sim.log
+echo --------------------------| tee -a /usr/local/scripts/sim.log
+LOG_FILE="/var/log/messages"  # Replace with the actual log file path
+KEYWORD="kernel"  # Replace with the message to trigger the reboot
 
+tail -f $LOG_FILE | while read LOGLINE; do
+    if [[ "$LOGLINE" == *"$KEYWORD"* ]]; then
+        echo "Failure message Found" | tee -a /usr/local/scripts/sim.log
+        echo "Rebooting system" | tee -a /usr/local/scripts/sim.log
+        sudo systemctl reboot
+    fi
+done
+exit
