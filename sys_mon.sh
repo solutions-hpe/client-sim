e02dbfc00e280e10320a5123a1da607955740e6e
Lance Bockenstedt <lrb@hpe.com>
2025-07-08 13:07:22 -0500

    Update sys_mon.sh

--- sys_mon.sh
+++ sys_mon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.03
+version=.04
 echo --------------------------| tee /usr/local/scripts/sim_reboot.log
 echo System Monitor Script Version $version | tee -a /usr/local/scripts/sim_reboot.log
 echo $(date) | tee -a /usr/local/scripts/sim_reboot.log
