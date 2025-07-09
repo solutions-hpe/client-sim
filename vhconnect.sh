0e2fd069d4afd377f79e59331278ea93e788a154
Lance Bockenstedt <lrb@hpe.com>
2025-07-09 13:27:03 -0500

    Update vhconnect.sh

--- vhconnect.sh
+++ vhconnect.sh
@@ -3,20 +3,30 @@ version=.15
 echo ------------------------------| tee -a /usr/local/scripts/sim.log
 echo VHConnect Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date)
+#----------------------------------------------------------------
 #Dumping Current Device List
+#----------------------------------------------------------------
 echo Getting VH device list
 sudo /usr/sbin/vhclientx86_64 -t LIST -r /tmp/vhactive.txt
+#----------------------------------------------------------------
 #Checking to see if there is a cache device to connect to
+#----------------------------------------------------------------
 echo VH Server is $vh_server
+#----------------------------------------------------------------
 #Counting & searching records in /tmp/vhactive.txt
+#----------------------------------------------------------------
 r_count=0
 y_count=0
+#----------------------------------------------------------------
 #Checking for the number of devices that are currently attached
+#----------------------------------------------------------------
 vhactive=$(cat /tmp/vhactive.txt | grep "*" | awk -F'[()]' '{print $2}')
 for r in $vhactive; do
 	y_count=$((y_count+1))
 done
+#----------------------------------------------------------------
 #Checking the number of devices that are available
+#----------------------------------------------------------------
 vhactive=$(cat /tmp/vhactive.txt | grep -e -- | grep -v In-use | awk -F'[()]' '{print $2}')
  for r in $vhactive; do
 	r_count=$((r_count+1))
@@ -31,33 +41,47 @@ if [ $r_count == 0 ]; then
 else
  if [ $vh_server == "on" ]; then
 	if [ -e "/usr/local/scripts/vhcached.txt" ]; then
+ 		#----------------------------------------------------------------
  		#Setting value to cached adapter
 		#This way the client is always using the same adapter
 		#Otherwise connectivity for clients will have gaps when the adapter changes in Central
+  		#----------------------------------------------------------------
 		vhserver_device=$(cat /usr/local/scripts/vhcached.txt)
 		echo Cached $vhserver_device
+  		#----------------------------------------------------------------
 		#If Client is connected to more than 1 device - disconnecting
+  		#----------------------------------------------------------------
 		if [[ $y_count -gt 1 ]]; then
 			echo Found multiple devices in-use
    			echo Clearing out all devices in-use
 			sudo /usr/sbin/vhclientx86_64 -t "AUTO USE CLEAR ALL"
 			sudo /usr/sbin/vhclientx86_64 -t "STOP USING ALL LOCAL"
 		fi
+	#----------------------------------------------------------------
   	#If VirtualHere cached value does not exist
+  	#----------------------------------------------------------------
    	else
-		#Generating random number to connect to a random adapter
+		#----------------------------------------------------------------
+  		#Generating random number to connect to a random adapter
+		#----------------------------------------------------------------
 		echo No Cached VH Device found - finding avaiable adapter
 		rn_vhactive=$((1 + RANDOM % $r_count))
+  		#----------------------------------------------------------------
 		#If Client is connected to more than 1 device - disconnecting
+  		#----------------------------------------------------------------
 		if [[ $y_count -gt 1 ]]; then
   			echo Found multiple devices in-use
 			echo Clearing out all devices in-use
 			sudo /usr/sbin/vhclientx86_64 -t "AUTO USE CLEAR ALL"
 			sudo /usr/sbin/vhclientx86_64 -t "STOP USING ALL LOCAL"
 		fi
+  		#----------------------------------------------------------------
 		#Resetting record counter for next loop
+  		#----------------------------------------------------------------
 		r_count=0
+  		#----------------------------------------------------------------
 		#Looping through records to find an available adapter
+  		#----------------------------------------------------------------
 		for r in $vhactive; do
 			r_count=$((r_count+1))
 			if [[ $r_count == $rn_vhactive ]]; then
@@ -66,21 +90,30 @@ else
 				echo $vhserver_device | tee /usr/local/scripts/vhcached.txt
 			fi
 		done
+  	#----------------------------------------------------------------
    	#End if VirtualHere cached value check
+    	#----------------------------------------------------------------
     fi
+    	#----------------------------------------------------------------
 	#End Checking to see if there is a cache device to connect to
 	#----------------------------------------------------------------
 	#----------------------------------------------------------------
 	#Connecting to VirtualHere Server
+ 	#----------------------------------------------------------------
 	echo Connecting to USB Adapter | tee -a /usr/local/scripts/sim.log
-	#Connecting to Adapter
+	#----------------------------------------------------------------	
+ 	#Connecting to Adapter
+ 	#----------------------------------------------------------------
 	/usr/sbin/vhclientx86_64 -t "AUTO USE PORT,$vhserver_device"
-	#End Connecting to VirtualHere Server
+	#----------------------------------------------------------------
+ 	#End Connecting to VirtualHere Server
+  	#----------------------------------------------------------------
  	echo Waiting for Adapter | tee -a /usr/local/scripts/sim.log
 	echo ------------------------------| tee -a /usr/local/scripts/sim.log
 	#----------------------------------------------------------------
 	sleep 30
+ #----------------------------------------------------------------
  #End if VirtualHere Server is enabled
+ #----------------------------------------------------------------
  fi
 fi
-#----------------------------------------------------------------
