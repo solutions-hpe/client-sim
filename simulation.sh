4f3a37eccb491b595c9d02347617d2cbab9c172a
Lance Bockenstedt <lrb@hpe.com>
2025-07-10 09:41:16 -0500

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -361,16 +361,6 @@ if [ $kill_switch == "off" ]; then
 	#------------------------------------------------------------
 	done
 else
-	#------------------------------------------------------------
-	#Running Cleanup from old simulations
- 	#------------------------------------------------------------
-	rm /usr/local/scripts/Contents*
-	rm /usr/local/scripts/main.cvd*
-	rm /usr/local/scripts/manifest*
- 	rm /tmp/Contents*
-	rm /tmp/main.cvd*
-	rm /tmp/manifest*
- 	rm /tmp/file.tmp
   	#------------------------------------------------------------
 	#If kill switch is enabled - sleeping for 5 minutes then restarting the loop
  	#------------------------------------------------------------
