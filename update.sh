6619af89fcea66f1faf3bac49605ba805d0e1f1b
Lance Bockenstedt <lrb@hpe.com>
2025-07-16 11:03:10 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version=.12
-echo ------------------------------| tee /usr/local/scripts/sim.log
+echo ------------------------------| tee -a /usr/local/scripts/sim.log
 echo Update Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
