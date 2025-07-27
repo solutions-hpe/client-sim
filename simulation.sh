efa45eb70c0e140306480626c18ff4c658214d62
Lance Bockenstedt <lrb@hpe.com>
2025-07-27 15:02:57 -0500

    Update Version

--- simulation.sh
+++ simulation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.77
+version=.78
 echo $(date) | tee -a /usr/local/scripts/sim.log
 echo ------------------------------| tee -a /usr/local/scripts/sim.log
 echo Simulation Script Version $version | tee -a /usr/local/scripts/sim.log
