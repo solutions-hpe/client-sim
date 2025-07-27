91395ed135c88dabcb159b93b81f35a78c133632
Lance Bockenstedt <lrb@hpe.com>
2025-07-26 20:26:11 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,8 @@ echo $(date) | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
 #Updating Scripts
 #------------------------------------------------------------
-#$repo_location=https://raw.githubusercontent.com/solutions-hpe/main/
+#repo_location=https://raw.githubusercontent.com/solutions-hpe/main/
+#public_repo=on
 echo Updating Scripts | tee -a /usr/local/scripts/sim.log
 github=raw.githubusercontent.com
 if [ $public_repo == "on" ]; then
