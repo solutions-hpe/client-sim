4bc49668a4d817c0371748051f6f4d6fe67704ef
Lance Bockenstedt <lrb@hpe.com>
2025-07-28 10:40:04 -0500

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -203,7 +203,7 @@ if [ $? -eq 0 ]; then
  echo Successful network connection | tee -a /usr/local/scripts/sim.log
 else
  echo Network connection failed | tee -a /usr/local/scripts/sim.log
- if [ $vh_server == "on" ]; then source '/usr/local/scripts/vhconnect.sh'; fi
+ if [ $vh_server == "on" ] && [ $ssidpw_fail != "on" ]; then source '/usr/local/scripts/vhconnect.sh'; fi
 fi
 #------------------------------------------------------------
 #End Connecting to VHServer
