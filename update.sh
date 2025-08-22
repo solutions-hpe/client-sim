36bed72475acbc6b1a56ddc00d835a45803aaf74
Lance Bockenstedt <lrb@hpe.com>
2025-08-22 10:08:28 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.17
+version=.18
 echo Update Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
@@ -26,29 +26,29 @@ if [ $public_repo == "on" ]; then
  ping -c1 $github
   if [ $? -eq 0 ]; then
    echo Successful network connection to Github - updating scripts
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"dns_fail.txt" -O /usr/local/scripts/dns_fail.txt
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"dns_fail.txt" -O /tmp/dns_fail.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"kill_switch.txt" -O /usr/local/scripts/kill_switch.txt
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"kill_switch.txt" -O /tmp/kill_switch.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"websites.txt" -O /usr/local/scripts/websites.txt
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"websites.txt" -O /tmp/websites.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"downloads.txt" -O /usr/local/scripts/downloads.txt
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"downloads.txt" -O /tmp/downloads.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"simulation.sh" -O /usr/local/scripts/simulation.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"simulation.sh" -O /tmp/simulation.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"startup.sh" -O /usr/local/scripts/startup.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"startup.sh" -O /tmp/startup.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"ini-parser.sh" -O /usr/local/scripts/ini-parser.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"ini-parser.sh" -O /tmp/ini-parser.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"sim-update.sh" -O /usr/local/scripts/sim-update.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"sim-update.sh" -O /tmp/sim-update.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"update.sh" -O /usr/local/scripts/update.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"update.sh" -O /tmp/update.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"vhconnect.sh" -O /usr/local/scripts/vhconnect.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"vhconnect.sh" -O /tmp/vhconnect.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"sys_mon.sh" -O /usr/local/scripts/sys_mon.sh
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"sys_mon.sh" -O /tmp/sys_mon.tmp
    sleep 1
-   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"configs/simulation.conf" -O /usr/local/scripts/simulation.conf
+   sudo wget --waitretry=10 --read-timeout=20 --timeout=15 $repo_location"configs/simulation.conf" -O /tmp/simulation.tmp
    sleep 1
    sudo chmod -R 777 /usr/local/scripts
   else
@@ -63,46 +63,46 @@ fi
 #------------------------------------------------------------
 echo Creating auto Start files | tee -a /tmp/client-sim.log
 #Creating Startup
-echo [Desktop Entry] | sudo tee /etc/xdg/autostart/startup.desktop
-echo Type=Application | sudo tee -a /etc/xdg/autostart/startup.desktop
-echo Name=StartUp | sudo tee -a /etc/xdg/autostart/startup.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /etc/xdg/autostart/startup.desktop
+echo [Desktop Entry] | sudo tee /tmp/startup.desktop
+echo Type=Application | sudo tee -a /tmp/startup.desktop
+echo Name=StartUp | sudo tee -a /tmp/startup.desktop
+echo Comment=Simulation Script Startup | sudo tee -a /tmp/startup.desktop
 #rasberrypi uses lxterminal
 #echo Exec=lxterminal -e bash /usr/local/scripts/startup.sh | sudo tee -a /etc/xdg/autostart/startup.desktop
 #Ubuntu/Debian with gnome
 #echo Exec=gnome-terminal --geometry=104x15+1400+477 -- bash -c /usr/local/scripts/startup.sh | sudo tee -a /etc/xdg/autostart/startup.desktop
-echo Exec=gnome-terminal --geometry=103x15+1400+525 -- bash -c "/usr/local/scripts/startup.sh ; systemctl reboot" | sudo tee -a /etc/xdg/autostart/startup.desktop
+echo Exec=gnome-terminal --geometry=103x15+1400+525 -- bash -c "/usr/local/scripts/startup.sh ; systemctl reboot" | sudo tee -a /tmp/startup.desktop
 #End Create Startup
 #------------------------------------------------------------
 #Create Log Viewer 
-echo [Desktop Entry] | sudo tee /etc/xdg/autostart/logview.desktop
-echo Type=Application | sudo tee -a /etc/xdg/autostart/logview.desktop
-echo Name=StartUp | sudo tee -a /etc/xdg/autostart/logview.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /etc/xdg/autostart/logview.desktop
+echo [Desktop Entry] | sudo tee /tmp/logview.desktop
+echo Type=Application | sudo tee -a /tmp/logview.desktop
+echo Name=StartUp | sudo tee -a /tmp/logview.desktop
+echo Comment=Simulation Script Startup | sudo tee -a /tmp/logview.desktop
 #rasberrypi uses lxterminal
 #echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
 #Ubuntu/Debian with gnome
 #echo Exec=gnome-terminal --geometry=20x15+0+477 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
-echo Exec=gnome-terminal --geometry=35x15+0+525 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
+echo Exec=gnome-terminal --geometry=35x15+0+525 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /tmp/logview.desktop
 #End Log Viewer
 #------------------------------------------------------------
 #Create journalctl Viewer 
-echo [Desktop Entry] | sudo tee /etc/xdg/autostart/journalctl.desktop
-echo Type=Application | sudo tee -a /etc/xdg/autostart/journalctl.desktop
-echo Name=StartUp | sudo tee -a /etc/xdg/autostart/journalctl.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /etc/xdg/autostart/journalctl.desktop
+echo [Desktop Entry] | sudo tee /tmp/journalctl.desktop
+echo Type=Application | sudo tee -a /tmp/journalctl.desktop
+echo Name=StartUp | sudo tee -a /tmp/journalctl.desktop
+echo Comment=Simulation Script Startup | sudo tee -a /tmp/journalctl.desktop
 #rasberrypi uses lxterminal
 #echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/journalctl.desktop
 #Ubuntu/Debian with gnome
-echo Exec=gnome-terminal --geometry=140x20+0+0 -- journalctl -f | sudo tee -a /etc/xdg/autostart/journalctl.desktop
+echo Exec=gnome-terminal --geometry=140x20+0+0 -- journalctl -f | sudo tee -a /tmp/journalctl.desktop
 #End Log Viewer#------------------------------------------------------------
 #Create journalctl Viewer 
-echo [Desktop Entry] | sudo tee /etc/xdg/autostart/sim_update.desktop
-echo Type=Application | sudo tee -a /etc/xdg/autostart/sim_update.desktop
-echo Name=StartUp | sudo tee -a /etc/xdg/autostart/sim_update.desktop
-echo Comment=Simulation Update | sudo tee -a /etc/xdg/autostart/sim_update.desktop
+echo [Desktop Entry] | sudo tee /tmp/sim_update.desktop
+echo Type=Application | sudo tee -a /tmp/sim_update.desktop
+echo Name=StartUp | sudo tee -a /tmp/sim_update.desktop
+echo Comment=Simulation Update | sudo tee -a /tmp/sim_update.desktop
 #rasberrypi uses lxterminal
 #echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/journalctl.desktop
 #Ubuntu/Debian with gnome
-echo Exec=gnome-terminal -- bash -c /usr/local/scripts/sim_update.sh | sudo tee -a /etc/xdg/autostart/sim_update.desktop
+echo Exec=gnome-terminal -- bash -c /usr/local/scripts/sim_update.sh | sudo tee -a /tmp/sim_update.desktop
 #End Log Viewer#------------------------------------------------------------
