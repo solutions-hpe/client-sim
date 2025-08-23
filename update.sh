b2580d0ac6b6270c2444a9b5b2ebc25b67626247
Lance Bockenstedt <lrb@hpe.com>
2025-08-23 10:27:05 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.19
+version=.20
 echo Update Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
@@ -24,15 +24,24 @@ echo Updating Scripts | tee -a /usr/local/scripts/sim.log
 if [ $public_repo == "on" ]; then
  #Using remote GitHub repo
  cd ~
+ #just in case the repo has not been cloned yet - attempting to clone the repo
+ #this will throw an error most of the time
  git clone $repo_location
  cd client-sim
+ #switching the branch to the one designated in the simulation.conf file
  git switch $repo_branch
+ #updating the local repo with fast forward option
  git pull origin --ff-only
+ #copying startup files to autostart
  sudo cp *.desktop /etc/xdg/autostart/
+ #copying shell scripts to the active script repo
  sudo cp *.sh /usr/local/scripts/
+ #copying flat files for simulation to active script repo
  sudo cp *.txt /usr/local/scripts/
  cd configs
+ #copying latest config file to active repository
  sudo cp simulation.conf /usr/local/scripts/simulation.conf
+ #making all simulation scripts executable
  sudo chmod -R 777 /usr/local/scripts
 else
  #Local repo defined in the conf file
