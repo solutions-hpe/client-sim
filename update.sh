efedb7490a92df4e8ff38f995c518a9d08d44029
Lance Bockenstedt <lrb@hpe.com>
2025-08-23 09:16:25 -0500

    Rev2 Updates

--- update.sh
+++ update.sh
@@ -23,12 +23,15 @@ echo Updating Scripts | tee -a /usr/local/scripts/sim.log
 if [ $public_repo == "on" ]; then
  #Using remote GitHub repo
  cd ~
+ git clone https://github.com/solutions-hpe/client-sim
  cd client-sim
+ git switch lrb
  git pull origin --ff-only
- cp *.sh /usr/local/scripts/
- cp *.txt /usr/local/scripts/
+ sudo cp *.desktop /etc/xdg/autostart/
+ sudo cp *.sh /usr/local/scripts/
+ sudo cp *.txt /usr/local/scripts/
  cd configs
- cp simulation.conf /usr/local/scripts/simulation.conf
+ sudo cp simulation.conf /usr/local/scripts/simulation.conf
  sudo chmod -R 777 /usr/local/scripts
 else
  #Local repo defined in the conf file
@@ -37,50 +40,3 @@ fi
 #------------------------------------------------------------
 #End Updating Scripts
 #------------------------------------------------------------
-echo Creating auto Start files | tee -a /tmp/client-sim.log
-#Creating Startup
-echo [Desktop Entry] | sudo tee /tmp/startup.desktop
-echo Type=Application | sudo tee -a /tmp/startup.desktop
-echo Name=StartUp | sudo tee -a /tmp/startup.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /tmp/startup.desktop
-#rasberrypi uses lxterminal
-#echo Exec=lxterminal -e bash /usr/local/scripts/startup.sh | sudo tee -a /etc/xdg/autostart/startup.desktop
-#Ubuntu/Debian with gnome
-#echo Exec=gnome-terminal --geometry=104x15+1400+477 -- bash -c /usr/local/scripts/startup.sh | sudo tee -a /etc/xdg/autostart/startup.desktop
-echo Exec=gnome-terminal --geometry=103x15+1400+525 -- bash -c "/usr/local/scripts/startup.sh ; systemctl reboot" | sudo tee -a /tmp/startup.desktop
-#End Create Startup
-#------------------------------------------------------------
-#Create Log Viewer 
-echo [Desktop Entry] | sudo tee /tmp/logview.desktop
-echo Type=Application | sudo tee -a /tmp/logview.desktop
-echo Name=StartUp | sudo tee -a /tmp/logview.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /tmp/logview.desktop
-#rasberrypi uses lxterminal
-#echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
-#Ubuntu/Debian with gnome
-#echo Exec=gnome-terminal --geometry=20x15+0+477 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/logview.desktop
-echo Exec=gnome-terminal --geometry=35x15+0+525 -- tail -f /usr/local/scripts/sim.log | sudo tee -a /tmp/logview.desktop
-#End Log Viewer
-#------------------------------------------------------------
-#Create journalctl Viewer 
-echo [Desktop Entry] | sudo tee /tmp/journalctl.desktop
-echo Type=Application | sudo tee -a /tmp/journalctl.desktop
-echo Name=StartUp | sudo tee -a /tmp/journalctl.desktop
-echo Comment=Simulation Script Startup | sudo tee -a /tmp/journalctl.desktop
-#rasberrypi uses lxterminal
-#echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/journalctl.desktop
-#Ubuntu/Debian with gnome
-echo Exec=gnome-terminal --geometry=140x20+0+0 -- journalctl -f | sudo tee -a /tmp/journalctl.desktop
-#End Log Viewer#------------------------------------------------------------
-#Create journalctl Viewer 
-echo [Desktop Entry] | sudo tee /tmp/sim_update.desktop
-echo Type=Application | sudo tee -a /tmp/sim_update.desktop
-echo Name=StartUp | sudo tee -a /tmp/sim_update.desktop
-echo Comment=Simulation Update | sudo tee -a /tmp/sim_update.desktop
-#rasberrypi uses lxterminal
-#echo Exec=lxterminal -t SIM-LOG-VIEWER --geometry=80x20 -e tail -f /usr/local/scripts/sim.log | sudo tee -a /etc/xdg/autostart/journalctl.desktop
-#Ubuntu/Debian with gnome
-echo Exec=gnome-terminal -- bash -c /usr/local/scripts/sim_update.sh | sudo tee -a /tmp/sim_update.desktop
-#End Log Viewer#------------------------------------------------------------
-cd /tmp
-sudo cp *.desktop /etc/xdg/autostart/
