9f2ec278ea3e32e971abae72ea8cc7709c6a8589
Lance Bockenstedt <lrb@hpe.com>
2025-08-31 09:41:25 -0500

    lrb

--- vhconnect.sh
+++ vhconnect.sh
@@ -116,7 +116,7 @@ else
  sleep 30
  wladapter=$(ip -br a | grep "wlx\|wlan" | cut -d ' ' -f '1')
  echo WLAN Adapter name $wladapter | tee -a /usr/local/scripts/sim.log
- sudo dhclient -k $wladapter
+ sudo dhclient -r $wladapter
  sleep 1
  sudo ip link set $wladapter down
  sleep 1
@@ -127,7 +127,7 @@ else
  echo Sleeping for 5 Seconds - Enable/Disable | tee -a /usr/local/scripts/sim.log
  echo Waiting for Network - Enable/Disable | tee -a /usr/local/scripts/sim.log
  sleep 5
- sudo dhclient -n $wladapter
+ sudo dhclient $wladapter
  #----------------------------------------------------------------
  #End if VirtualHere Server is enabled
  #----------------------------------------------------------------
