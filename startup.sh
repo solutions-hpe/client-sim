9a0674bdd3e6c9975f9ed1f5faa69da2ceee4aba
Lance Bockenstedt <lrb@hpe.com>
2025-08-31 10:28:27 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -79,15 +79,11 @@ echo Bringing up all interfaces online | tee -a /usr/local/scripts/sim.log
 #Finding adapter names and setting usable variables for interfaces
 #------------------------------------------------------------
 wladapter=$(ip -br a | grep "wlx\|wlan" | cut -d ' ' -f '1')
-var_length=${#wladapter}
-if [ $var_length -lt '1' ]; then echo test-lance-wl; fi
 eadapter=$(ip -br a | grep "enp\|eno\|eth0\|eth1\|eth2\|eth3\|eth4\|eth5\|eth6\|ens" | cut -d ' ' -f '1')
-var_length=${#eadapter}
-if [ $var_length -lt '1' ]; then echo test-lance-ea; fi
-if [ -n ${wladapter} ]; then echo WLAN Adapter name $wladapter | tee -a /usr/local/scripts/sim.log; fi
-if [ -n ${eadapter} ]; then echo Wired Adapter name $eadapter | tee -a /usr/local/scripts/sim.log; fi
-if [ -n ${wladapter} ]; then sudo ip link set dev $wladapter up; fi
-if [ -n ${eadapter} ]; then sudo ip link set dev $eadapter up; fi
+if [ ${#wladapter} -lt '1' ]; then echo WLAN Adapter name $wladapter | tee -a /usr/local/scripts/sim.log; fi
+if [ ${#eadapter} -lt '1' ]; then echo Wired Adapter name $eadapter | tee -a /usr/local/scripts/sim.log; fi
+if [ ${#wladapter} -lt '1' ]; then sudo ip link set dev $wladapter up; fi
+if [ ${#eadapter} -lt '1' ]; then sudo ip link set dev $eadapter up; fi
 echo -----------------------------| tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
 #Running Updates
