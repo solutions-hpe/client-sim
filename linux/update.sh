8d96ebf0506cb239605033de40496e7cff33ecae
Lance Bockenstedt <lrb@hpe.com>
2026-04-24 11:56:53 -0500

    Simulation Update

--- linux/update.sh
+++ linux/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=.22
+version=.23
 echo Update Script Version $version | tee -a /usr/local/scripts/sim.log
 echo $(date) | tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
