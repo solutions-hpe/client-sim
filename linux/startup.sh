39175809dfc33a7f5d41097ecd80157aabc165b4
Lance Bockenstedt <lrb@hpe.com>
2026-04-24 11:45:05 -0500

    Simulation Update

--- linux/startup.sh
+++ linux/startup.sh
@@ -118,13 +118,8 @@ echo -----------------------------| tee -a /usr/local/scripts/sim.log
 #------------------------------------------------------------
 #Running Updates
 #------------------------------------------------------------
-if [ $rapid_update != "on" ]; then
- echo Updating Simulation from repo | tee -a /usr/local/scripts/sim.log
- source '/usr/local/scripts/update.sh'
-else
- echo Rapid Update is $rapid_update | tee -a /usr/local/scripts/sim.log
- echo Skipping update | tee -a /usr/local/scripts/sim.log
-fi
+echo Updating Simulation from repo | tee -a /usr/local/scripts/sim.log
+source '/usr/local/scripts/update.sh'
 #------------------------------------------------------------
 #Setting VirtualHere Server as a Daemon
 #------------------------------------------------------------
