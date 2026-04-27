82df7fd40e281f3e4deb6ae8cbebb14befcc95f6
Lance Bockenstedt <lrb@hpe.com>
2026-04-27 07:11:52 -0500

    Migrated Linux to windows

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo mkdir /usr/local/scripts
 #Without this - the logging screens at boot will not be pinned to the right x,y coordinates
 echo Disabling Wayland so gnome-terminal windows can be pinned | tee -a /tmp/client-sim.log
 sudo sed -i '/WaylandEnable=false/s/^#//g' /etc/gdm3/custom.conf
-sudo raspi-config nonint do_wayland 1
+sudo mkdir -p /etc/lightdm/lightdm.conf.d && printf "[Seat:*]\ndisplay-server=xorg\nuser-session=LXDE-pi-x\n" | sudo tee /etc/lightdm/lightdm.conf.d/99-force-x11.conf >/dev/null
 #By default screen will blank and need to log back in after 5 minutes - disabling this as the client is running scripts
 echo Disabling screen blanking | tee -a /tmp/client-sim.log
 gsettings set org.gnome.desktop.session idle-delay 0
