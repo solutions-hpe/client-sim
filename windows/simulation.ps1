431fcb636cbb91f58f08a8ddc08a31fc6c42605c
Lance Bockenstedt <lrb@hpe.com>
2026-05-02 11:00:55 -0500

    Merge remote-tracking branch 'origin/lrb' into lrb
    # Conflicts:
    #	windows/simulation.ps1

--- windows/simulation.ps1
+++ windows/simulation.ps1
@@ -1,194 +1,210 @@
-# PowerShell equivalent of simulation.sh
 . .\ini-parser.ps1
 
 $version = "0.91"
 $logPath = "C:\Scripts\sim.log"
-Get-Date | Tee-Object -FilePath $logPath -Append
-"------------------------------" | Tee-Object -FilePath $logPath -Append
-"Simulation Script Version $version" | Tee-Object -FilePath $logPath -Append
 
-# Finding adapter names
-$wladapter = Get-NetAdapter | Where-Object { $_.Name -like "*wireless*" -or $_.Name -like "*wlan*" } | Select-Object -First 1 -ExpandProperty Name
-if ($wladapter) { "WLAN Adapter name $wladapter" | Tee-Object -FilePath $logPath -Append }
-$eadapter = Get-NetAdapter | Where-Object { $_.Name -like "*ethernet*" -or $_.Name -like "*eth*" } | Select-Object -First 1 -ExpandProperty Name
-if ($eadapter) { "Wired Adapter name $eadapter" | Tee-Object -FilePath $logPath -Append }
+function Log($msg) {
+    $msg | Tee-Object -FilePath $logPath -Append
+}
 
-"Parsing Config File" | Tee-Object -FilePath $logPath -Append
-$global:iniConfig = Parse-IniFile 'C:\Scripts\simulation.conf'
+function Get-SafeInt($v, $default = 0) {
+    try {
+        if ($null -eq $v -or $v -eq "") { return $default }
+        return [int]$v
+    } catch {
+        return $default
+    }
+}
 
-# Global Simulation settings
-$kill_switch = get_value 'simulation' 'kill_switch'
-$rapid_update = get_value 'simulation' 'rapid_update'
-$sim_load = get_value 'simulation' 'sim_load'
-$public_repo = get_value 'simulation' 'public_repo'
-$repo_location = get_value 'simulation' 'repo_location'
-$vh_server = get_value 'simulation' 'vh_server'
-$site_based_ssid = get_value 'simulation' 'site_based_ssid'
-$iperf_bw = get_value 'simulation' 'iperf_bw'
-$auth_fail = get_value 'simulation' 'auth_fail'
-$ssidpw_fail = get_value 'simulation' 'ssidpw_fail'
-$allow_offline = get_value 'simulation' 'allow_offline'
+function Get-SafeString($v, $default = "") {
+    if ([string]::IsNullOrWhiteSpace($v)) { return $default }
+    return [string]$v
+}
 
-# Device Specific
-$wsite = get_value $simulation_id 'wsite'
-$sim_phy = get_value $simulation_id 'sim_phy'
-$ssid = get_value $simulation_id 'ssid'
-$ssidpw = get_value $simulation_id 'ssidpw'
-$dhcp_fail = get_value $simulation_id 'dhcp_fail'
-$dns_fail = get_value $simulation_id 'dns_fail'
-$assoc_fail = get_value $simulation_id 'assoc_fail'
-$port_flap = get_value $simulation_id 'port_flap'
-$ping_test = get_value $simulation_id 'ping_test'
-$download = get_value $simulation_id 'download'
-$iperf = get_value $simulation_id 'iperf'
-$www_traffic = get_value $simulation_id 'www_traffic'
+Get-Date | Log
+"------------------------------" | Log
+"Simulation Script Version $version" | Log
 
-# Simulation IP
-$smb_address = get_value 'address' 'smb_address'
-$ping_address = get_value 'address' 'ping_address'
-$dns_latency_1 = get_value 'address' 'dns_latency_1'
-$dns_latency_2 = get_value 'address' 'dns_latency_2'
-$dns_latency_3 = get_value 'address' 'dns_latency_3'
-$dns_bad_ip_1 = get_value 'address' 'dns_bad_ip_1'
-$dns_bad_ip_2 = get_value 'address' 'dns_bad_ip_2'
-$dns_bad_ip_3 = get_value 'address' 'dns_bad_ip_3'
-$dns_bad_record_1 = get_value 'address' 'dns_bad_record_1'
-$dns_bad_record_2 = get_value 'address' 'dns_bad_record_2'
-$dns_bad_record_3 = get_value 'address' 'dns_bad_record_3'
-$vh_server_address = get_value 'address' 'vh_server_addr'
-$iperf_server = get_value 'address' 'iperf_server'
+$iniConfig = Parse-IniFile 'C:\Scripts\simulation.conf'
 
-# User overrides (assuming $username is set)
-$tempvar = get_value $username 'kill_switch'
-if ($tempvar) { $kill_switch = $tempvar }
-# ... similarly for others
+$hostname = $env:COMPUTERNAME
+if ([string]::IsNullOrWhiteSpace($hostname)) { $hostname = $env:HOSTNAME }
+if ([string]::IsNullOrWhiteSpace($hostname)) { $hostname = [System.Net.Dns]::GetHostName() }
+if ([string]::IsNullOrWhiteSpace($hostname)) { $hostname = "UNKNOWN" }
 
-Get-Date | Tee-Object -FilePath $logPath -Append
-"------------------------------" | Tee-Object -FilePath $logPath -Append
-"Simulation Details:" | Tee-Object -FilePath $logPath -Append
-"Hostname: $env:COMPUTERNAME"   | Tee-Object -FilePath $logPath -Append
-"Site: $wsite" | Tee-Object -FilePath $logPath -Append
-"Site Based SSID: $site_based_ssid" | Tee-Object -FilePath $logPath -Append
-if ($vh_server -eq "off") { "Phy: $sim_phy" | Tee-Object -FilePath $logPath -Append }
-if ($sim_phy -eq "wireless" -and $wladapter) { "Adapter: $wladapter" | Tee-Object -FilePath $logPath -Append }
-"Simulation Load: $sim_load" | Tee-Object -FilePath $logPath -Append
-"Kill Switch: $kill_switch" | Tee-Object -FilePath $logPath -Append
-"DHCP Fail: $dhcp_fail" | Tee-Object -FilePath $logPath -Append
-"DNS Fail: $dns_fail" | Tee-Object -FilePath $logPath -Append
-"WWW Traffic: $www_traffic" | Tee-Object -FilePath $logPath -Append
-"iPerf: $iperf" | Tee-Object -FilePath $logPath -Append
-"Download: $download" | Tee-Object -FilePath $logPath -Append
-"Port Flap: $port_flap" | Tee-Object -FilePath $logPath -Append
-"Incorrect SSID PW: $ssidpw_fail" | Tee-Object -FilePath $logPath -Append
-"------------------------------" | Tee-Object -FilePath $logPath -Append
-Start-Sleep 5
+Log "Hostname: $hostname"
 
-# Global kill switch
-$kill_switch = Get-Content 'C:\Scripts\kill_switch.txt'
+$wladapter = Get-NetAdapter | Where-Object { $_.Name -match "wireless|wlan|wi-fi" } | Select-Object -First 1 -ExpandProperty Name
+$eadapter  = Get-NetAdapter | Where-Object { $_.Name -match "ethernet|eth|enp|eno|ens" } | Select-Object -First 1 -ExpandProperty Name
 
-# Random numbers
-$rn = Get-Random -Minimum 1 -Maximum 61
-$rn_iperf_port = 5201 + (Get-Random -Minimum 0 -Maximum 11)
-$rn_iperf_time = Get-Random -Minimum 1 -Maximum 301
-$rn_ping_size = Get-Random -Minimum 1 -Maximum 65001
-$rn_offline_time = Get-Random -Minimum 1 -Maximum 14401
+if ($wladapter) { Log "WLAN Adapter name $wladapter" }
+if ($eadapter) { Log "Wired Adapter name $eadapter" }
+
+Log "Parsing Config File"
+
+$kill_switch = Get-SafeString (get_value 'simulation' 'kill_switch') "off"
+$rapid_update = Get-SafeString (get_value 'simulation' 'rapid_update') "off"
+$sim_load = Get-SafeInt (get_value 'simulation' 'sim_load') 100
+$vh_server = Get-SafeString (get_value 'simulation' 'vh_server') "off"
+$site_based_ssid = Get-SafeString (get_value 'simulation' 'site_based_ssid') "off"
+$ssidpw_fail = Get-SafeString (get_value 'simulation' 'ssidpw_fail') "off"
+$auth_fail = Get-SafeString (get_value 'simulation' 'auth_fail') "off"
+$dns_fail = Get-SafeString (get_value 'simulation' 'dns_fail') "off"
+$download = Get-SafeString (get_value 'simulation' 'download') "off"
+$iperf = Get-SafeString (get_value 'simulation' 'iperf') "off"
+$www_traffic = Get-SafeString (get_value 'simulation' 'www_traffic') "off"
+
+$wsite = Get-SafeString (get_value $simulation_id 'wsite')
+$sim_phy = Get-SafeString (get_value $simulation_id 'sim_phy') "wireless"
+$ssid = Get-SafeString (get_value $simulation_id 'ssid')
+$ssidpw = Get-SafeString (get_value $simulation_id 'ssidpw')
+
+$ping_address = Get-SafeString (get_value 'address' 'ping_address')
+$iperf_server = Get-SafeString (get_value 'address' 'iperf_server')
+
+$dfgw = Get-NetRoute -DestinationPrefix "0.0.0.0/0" |
+        Sort-Object RouteMetric |
+        Select-Object -First 1 -ExpandProperty NextHop
+
+Log "DEBUG: Default gateway = [$dfgw]"
+
+$network_ok = $false
+
+if ([string]::IsNullOrWhiteSpace($dfgw)) {
+    Log "No default gateway detected"
+    $network_ok = $false
+}
+else {
+    try {
+        $network_ok = Test-Connection -ComputerName $dfgw -Count 2 -Quiet -ErrorAction Stop
+    } catch {
+        Log "Gateway test failed: $($_.Exception.Message)"
+        $network_ok = $false
+    }
+}
+
+if ($network_ok) {
+    Log "Successful network connection"
+}
+else {
+    Log "Network connection failed"
+
+    if ($vh_server -eq "on" -and (Test-Path .\vhconnect.ps1)) {
+        . .\vhconnect.ps1
+    }
+
+    Start-Sleep 15
+
+    if ($site_based_ssid -eq "on" -and $ssid -and $wsite) {
+        netsh wlan connect name="$wsite-$ssid"
+    }
+    elseif ($ssid) {
+        netsh wlan connect name="$ssid"
+    }
+
+    Start-Sleep 15
+}
+
+if ($rapid_update -eq "on" -and (Test-Path .\update.ps1)) {
+    . .\update.ps1
+}
+
+Log "Disabling unused interface"
+
+if ($sim_phy -eq "ethernet" -and $wladapter) {
+    Disable-NetAdapter -Name $wladapter -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+if ($sim_phy -eq "wireless" -and $vh_server -eq "off" -and $eadapter) {
+    Disable-NetAdapter -Name $eadapter -Confirm:$false -ErrorAction SilentlyContinue
+}
+
 $rn_sim_load = Get-Random -Minimum 1 -Maximum 100
 
-# Running update if rapid_update on
-if ($rapid_update -eq "on") { . .\update.ps1 }
-
-# Disabling unused interface
-"Disabling unused interface" | Tee-Object -FilePath $logPath -Append
-if ($sim_phy -eq "ethernet") { Disable-NetAdapter -Name $wladapter -Confirm:$false }
-if ($sim_phy -eq "wireless" -and $vh_server -eq "off") { Disable-NetAdapter -Name $eadapter -Confirm:$false }
-
-# MAC ID (simplified)
-$mac_id = $env:COMPUTERNAME.Substring($env:COMPUTERNAME.Length - 4, 2) + ":" + $env:COMPUTERNAME.Substring($env:COMPUTERNAME.Length - 2, 2)
-
-# Connecting to VHServer (simplified)
-$dfgw = (Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop
-if (Test-Connection -ComputerName $dfgw -Count 2 -Quiet) {
-    "Successful network connection" | Tee-Object -FilePath $logPath -Append
-} else {
-    "Network connection failed" | Tee-Object -FilePath $logPath -Append
-    if ($vh_server -eq "on") { . .\vhconnect.ps1 }
-    Start-Sleep 15
-    # Connect WiFi using netsh
-    if ($site_based_ssid -eq "on") { netsh wlan connect name="$wsite-$ssid" }
-    else { netsh wlan connect name="$ssid" }
-    Start-Sleep 15
-    $dfgw = (Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop
+if ($sim_load -lt $rn_sim_load) {
+    Log "Simulation load under threshold"
+    Start-Sleep (Get-Random -Minimum 1 -Maximum 10)
 }
 
-# Simulation load
-if ([int]$sim_load -lt $rn_sim_load) {
-    "Simulation load under threshold" | Tee-Object -FilePath $logPath -Append
-    "Skipping Simulations but staying associated" | Tee-Object -FilePath $logPath -Append
-    netsh wlan set hostednetwork mode=disallow
-    Start-Sleep $rn_offline_time
-    netsh wlan set hostednetwork mode=allow
-    Start-Sleep 5
-    if ($site_based_ssid -eq "on" -and $ssidpw_fail -ne "on" -and $wladapter) { netsh wlan connect name="$wsite-$ssid" }
-    if ($site_based_ssid -ne "on" -and $ssidpw_fail -ne "on" -and $wladapter) { netsh wlan connect name="$ssid" }
-    Start-Sleep 5
-}
+$kill_switch = Get-SafeString $kill_switch "off"
 
-"Kill Switch is $kill_switch" | Tee-Object -FilePath $logPath -Append
+Log "Kill Switch is $kill_switch"
+
 if ($kill_switch -eq "off") {
+
     for ($z = 1; $z -le 100; $z++) {
-        # SSID PW Fail or Auth Fail
+
         if (($ssidpw_fail -eq "on" -or $auth_fail -eq "on") -and $wladapter) {
-            if ($ssidpw_fail -eq "on") {
+
+            if ($ssidpw_fail -eq "on" -and $ssid) {
                 for ($i = 1; $i -le 100; $i++) {
-                    "Running SSID Incorrect Password" | Tee-Object -FilePath $logPath -Append
-                    "Iteration $i of 100" | Tee-Object -FilePath $logPath -Append
-                    # Delete connections (simplified)
-                    if ($site_based_ssid -eq "on" -or $ssidpw_fail -eq "on") { netsh wlan connect name="$wsite-$ssid" }
-                    else { netsh wlan connect name="$ssid" }
+                    Log "SSID Incorrect Password Simulation"
+                    if ($site_based_ssid -eq "on" -and $wsite) {
+                        netsh wlan connect name="$wsite-$ssid"
+                    }
+                    elseif ($ssid) {
+                        netsh wlan connect name="$ssid"
+                    }
                 }
             }
-            if ($auth_fail -eq "on") {
-                "Running Auth Failure" | Tee-Object -FilePath $logPath -Append
+
+            if ($auth_fail -eq "on" -and $wladapter) {
                 for ($i = 1; $i -le 100; $i++) {
-                    "Enable/Disable WLAN interface" | Tee-Object -FilePath $logPath -Append
-                    "Iteration $i of 100" | Tee-Object -FilePath $logPath -Append
-                    Disable-NetAdapter -Name $wladapter -Confirm:$false
-                    Start-Sleep 5
-                    Enable-NetAdapter -Name $wladapter
+                    Disable-NetAdapter -Name $wladapter -Confirm:$false -ErrorAction SilentlyContinue
+                    Start-Sleep 2
+                    Enable-NetAdapter -Name $wladapter -ErrorAction SilentlyContinue
                 }
             }
-        } else {
-            if (Test-Connection -ComputerName $dfgw -Count 2 -Quiet) {
-                "Successful network connection" | Tee-Object -FilePath $logPath -Append
-            } else {
-                "Network connection failed" | Tee-Object -FilePath $logPath -Append
-                "Attempting to reset adapter" | Tee-Object -FilePath $logPath -Append
-                if ($vh_server -eq "on") { . .\vhconnect.ps1 }
+        }
+        else {
+
+            if ($network_ok) {
+                Log "Successful network connection"
+            }
+            else {
+                Log "Network connection failed"
+
+                if ($vh_server -eq "on" -and (Test-Path .\vhconnect.ps1)) {
+                    . .\vhconnect.ps1
+                }
+
                 Start-Sleep 15
-                # Reconnect
-                if ($site_based_ssid -eq "on") { netsh wlan connect name="$wsite-$ssid" }
-                else { netsh wlan connect name="$ssid" }
+
+                if ($site_based_ssid -eq "on" -and $ssid -and $wsite) {
+                    netsh wlan connect name="$wsite-$ssid"
+                }
+                elseif ($ssid) {
+                    netsh wlan connect name="$ssid"
+                }
+
                 Start-Sleep 15
             }
-            $dfgw = (Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop
-            if (Test-Connection -ComputerName $dfgw -Count 2 -Quiet) {
-                "Successful network connection" | Tee-Object -FilePath $logPath -Append
-            } else {
-                "Connection failed multiple times" | Tee-Object -FilePath $logPath -Append
-                "Resetting configuration" | Tee-Object -FilePath $logPath -Append
-                "Purging VHConfig" | Tee-Object -FilePath $logPath -Append
-                # VH commands
-                & 'vhclientx86_64.exe' -t "STOP USING ALL LOCAL"
-                & 'vhclientx86_64.exe' -t "AUTO USE CLEAR ALL"
+
+            $dfgw = Get-NetRoute -DestinationPrefix "0.0.0.0/0" |
+                    Sort-Object RouteMetric |
+                    Select-Object -First 1 -ExpandProperty NextHop
+
+            if (-not [string]::IsNullOrWhiteSpace($dfgw)) {
+                $network_ok = Test-Connection -ComputerName $dfgw -Count 2 -Quiet -ErrorAction SilentlyContinue
+            }
+            else {
+                $network_ok = $false
+            }
+
+            if (-not $network_ok) {
+                Log "Connection failed multiple times"
+
+                if (Test-Path vhclientx86_64.exe) {
+                    & 'vhclientx86_64.exe' -t "STOP USING ALL LOCAL"
+                    & 'vhclientx86_64.exe' -t "AUTO USE CLEAR ALL"
+                }
             }
         }
 
-        # Other simulations
-        if ($dns_fail -eq "on") { . .\dns_fail.ps1 }
-        if ($download -eq "on") { . .\download.ps1 }
-        if ($iperf -eq "on") { . .\iperf.ps1 }
-        # Add others as needed
+        if ($dns_fail -eq "on" -and (Test-Path .\dns_fail.ps1)) { . .\dns_fail.ps1 }
+        if ($download -eq "on" -and (Test-Path .\download.ps1)) { . .\download.ps1 }
+        if ($iperf -eq "on" -and (Test-Path .\iperf.ps1)) { . .\iperf.ps1 }
 
-        Start-Sleep $rn
+        Start-Sleep (Get-SafeInt $rn_sim_load 5)
     }
 }
