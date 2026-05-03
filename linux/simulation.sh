81981d159e678ad071b4619998b5b66ad46eae34
Lance Bockenstedt <lrb@hpe.com>
2026-05-03 17:20:57 -0500

    Merge remote-tracking branch 'origin/lrb' into lrb
    # Conflicts:
    #	windows/simulation.ps1

--- linux/simulation.sh
+++ linux/simulation.sh
@@ -376,11 +376,31 @@ if [ $kill_switch == "off" ]; then
    #------------------------------------------------------------
    #End Connecting to Network
    #------------------------------------------------------------
-   if [ $www_traffic == "on" ]; then run_simulation "www_traffic.sh" 30; fi
-   if [ $ping_test == "on" ]; then run_simulation "ping_test.sh" 30; fi
-   if [ $iperf == "on" ]; then run_simulation "iperf.sh" 30; fi
-   if [ $download == "on" ]; then run_simulation "download.sh" 30; fi
-   if [ $dns_fail == "on" ]; then run_simulation "dns_fail.sh" 30; fi
+   if [ "$www_traffic" == "on" ]; then
+    if ! pgrep -f "www_traffic.sh" >/dev/null; then
+     run_simulation "www_traffic.sh" 30
+    fi
+   fi
+   if [ "$ping_test" == "on" ]; then
+    if ! pgrep -f "ping_test.sh" >/dev/null; then
+     run_simulation "ping_test.sh" 30
+    fi
+   fi
+   if [ "$iperf" == "on" ]; then
+    if ! pgrep -f "iperf.sh" >/dev/null; then
+     run_simulation "iperf.sh" 30
+    fi
+   fi
+   if [ "$download" == "on" ]; then
+    if ! pgrep -f "download.sh" >/dev/null; then
+     run_simulation "download.sh" 30
+    fi
+   fi
+   if [ "$dns_fail" == "on" ]; then
+    if ! pgrep -f "dns_fail.sh" >/dev/null; then
+     run_simulation "dns_fail.sh" 30
+    fi
+   fi
    echo End of simulation | tee -a $log
    #------------------------------------------------------------
    #Running update to either the cloud repo or local SMB repo
