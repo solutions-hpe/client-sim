cc1ac0bbd8ceae49cdfe1fa19e906cc3572c4b43
Lance Bockenstedt <lrb@hpe.com>
2026-05-03 17:09:50 -0500

    Merge remote-tracking branch 'origin/lrb' into lrb
    # Conflicts:
    #	windows/simulation.ps1

--- linux/simulation.sh
+++ linux/simulation.sh
@@ -256,9 +256,9 @@ dfgw=$(ip route | grep -oP 'default via \K\S+')
 echo Ping the Default Gateway | tee -a $log
 ping -c2 $dfgw
 if [ $? -eq 0 ] && [ $sim_phy == "wireless" ] && [[ -n ${wladapter} ]]; then
- echo Successful network connection | tee -a $log
+ echo Successful network connection - Pre-Simulation | tee -a $log
 else
-  echo Network connection failed | tee -a $log
+  echo Network connection failed - Pre-Simulation | tee -a $log
   #------------------------------------------------------------
   #If VH is enabled then attempt to connect to VHServer
   #------------------------------------------------------------
@@ -329,11 +329,12 @@ if [ $kill_switch == "off" ]; then
    #If SSID Incorrect Password Sim is not triggered then check
    #for the other simualtions
    #------------------------------------------------------------
+   dfgw=$(ip route | grep -oP 'default via \K\S+')
    ping -c2 $dfgw
    if [ $? -eq 0 ]; then
-    echo Successful network connection | tee -a $log
+    echo Successful network connection - Simulation Loop | tee -a $log
     else
-     echo Network connection failed | tee -a $log
+     echo Network connection failed - Simulation Loop | tee -a $log
      echo Attempting to reset adapter | tee -a $log
      if [ $vh_server == "on" ]; then source '/usr/local/scripts/vhconnect.sh'; fi
      sleep 15
@@ -343,10 +344,9 @@ if [ $kill_switch == "off" ]; then
      echo WLAN Adapter name $wladapter | tee -a $log
      sleep 15
     fi
-    dfgw=$(ip route | grep -oP 'default via \K\S+')
     ping -c2 $dfgw
     if [ $? -eq 0 ]; then
-     echo Successful network connection | tee -a $log
+     echo Successful network connection - After Adapter Reset | tee -a $log
     else
     echo Connection failed muiltiple times | tee -a $log
     echo Resetting configuration | tee -a $log
