8c9ba8466527235d4860dde0d4e4fa429e70d67c
Lance Bockenstedt <lrb@hpe.com>
2026-05-03 21:16:54 -0500

    Merge remote-tracking branch 'origin/lrb' into lrb
    # Conflicts:
    #	windows/simulation.ps1

--- linux/www_traffic.sh
+++ linux/www_traffic.sh
@@ -4,4 +4,4 @@ log="/usr/local/scripts/sim.log"
 wwwfile=($(< /usr/local/scripts/websites.txt))
 rn_www=$((RANDOM % ${#wwwfile[@]}))
 url="${wwwfile[$rn_www]}"
-firefox --headless "$url"
+cpulimit -l 25 -- firefox --headless "$url"
