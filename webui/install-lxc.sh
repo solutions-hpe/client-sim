746ae54ce71451e922c34f74b3afd481817936f2
Lance Bockenstedt <lrb@hpe.com>
2026-05-08 22:39:21 -0500

    Move Update Agent button to far right of header, Last seen beside it

--- webui/install-lxc.sh
+++ webui/install-lxc.sh
@@ -127,7 +127,7 @@ if [[ -z "${_CLIENT_SIM_BOOTSTRAPPED:-}" ]]; then
   exit $?
 fi
 
-VERSION="1.00"
+VERSION="1.01"
 INSTALL_START=$(date +%s)
 MODE="Update"
 [[ "$REINSTALL" -eq 1 ]] && MODE="Full Reinstall"
